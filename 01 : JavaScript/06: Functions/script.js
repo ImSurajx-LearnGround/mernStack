69bd9a26a59a0061656f18395d77ff7b3400e816
imsurajx <surajxcode@gmail.com>
2025-11-05 02:00:17 +0530

    JavaScript : Closure Problems
    Signed-off-by: imsurajx <surajxcode@gmail.com>

--- 01 : JavaScript/06: Functions/script.js
+++ 01 : JavaScript/06: Functions/script.js
@@ -175,7 +175,7 @@ function abnc(val){
 abnc(
     function(){
         console.log("Hello Argument Function.");
-        
+
     }
 );
 
@@ -300,3 +300,231 @@ console.log(double(5));
 console.log(password);
 
 
+CLOSURE : QUESTIONS 
+
+// Goal: Build a counter that remembers its previous value.
+function createCounter(){
+    let count = 0;
+    return function(){
+        count++;
+        console.log(count);
+    }
+}
+const counter = createCounter();
+counter();
+counter();
+counter();
+
+// Goal: Learn how closures hide data.
+function bankAccount(){
+    let balance = 0;
+    return function(value,amount){
+        if (value == "deposit") balance += amount;
+        else if(value == "withdraw") balance -= amount;
+        else if (value == "check") return console.log(`balance is ${balance}`);
+    }
+}
+const account = bankAccount();
+account("deposit", 1000);
+account("deposit", 500);
+account("withdraw", 100);
+account("withdraw", 50);
+account("check");
+
+// Goal: Understand how closures can store custom data (like a name) and use it later inside another function.
+function greetMaker(name){
+    return function(){
+        console.log(`Hello, ${name}`);
+    }
+}
+
+const greet = greetMaker("Suraj");
+greet();
+
+// 🎯 Goal: Create a function that can generate unique IDs each time it’s called — and remember the last one it gave out.
+function idGenerator(){
+    let id = 0;
+    return function(){
+        id++;
+        return id;
+    }
+}
+const userId = idGenerator();
+const orderId = idGenerator();
+console.log(`user id is: ${userId()}`);
+console.log(`user id is: ${userId()}`);
+console.log(`user id is: ${userId()}`);
+console.log(`user id is: ${userId()}`);
+console.log(`order id is: ${orderId()}`);
+console.log(`order id is: ${orderId()}`);
+
+
+// 🎯 Goal: Create a reusable discount function using closures and higher-order functions. You should be able to make multiple discount calculators (like 10%, 20%, etc.) easily.
+function createDiscount(percent){
+    return function(amount){
+        amount -= (percent / 100) * amount;
+        return amount;
+    }
+}
+let discount10 = createDiscount(10);
+let discount20 = createDiscount(20);
+let discount30 = createDiscount(30);
+console.log(discount10(1000));
+console.log(discount20(1200));
+console.log(discount30(1300));
+
+// 🎯 Goal: Create a higher-order function that can generate power calculators using closures.
+function poweOf(exponent){
+    return function(raise){
+        let product = 1;
+        for(let i = 1; i <= exponent; i++){
+            product *= raise;
+        }
+        return product;
+    }
+}
+
+let square = poweOf(2);
+let cube = poweOf(3);
+let quad = poweOf(4);
+
+console.log(square(5));
+console.log(cube(5));
+console.log(quad(5));
+
+// 🎯 Goal: Create a reusable tip calculator that uses closures to remember a specific tip percentage.
+function tipCalculator(tipPercent){
+    return function(amount){
+        let tipAmount = 0
+        tipAmount += (tipPercent / 100) * amount;
+        return tipAmount;
+    }
+}
+let tip10 = tipCalculator(10);
+let tip15 = tipCalculator(15);
+let tip20 = tipCalculator(20);
+console.log(tip10(1000));
+console.log(tip15(1200));
+console.log(tip20(1300));
+
+// 🎯 Goal: Create a function generator that remembers a base value and returns a new function that adds numbers to that base.
+function makeAdder(baseValue){
+    return function(num){
+        return baseValue + num;
+    }
+}
+
+const add5 = makeAdder(5);
+const add10 = makeAdder(10);
+const add20 = makeAdder(20);
+
+console.log(add5(18));
+console.log(add5(39));
+console.log(add10(28));
+console.log(add20(999));
+
+// 🎯 Goal: Create a function that limits how many times a specific action (like accessing something) can be performed. Once the limit is reached, it should deny further access.
+function limitAccess(times) {
+    let count = 0;
+    return function () {
+        if (count < times) {
+            count++;
+            return console.log("Access Granted");
+        } else {
+            return console.log("Access Declined");
+        }
+    }
+}
+const access = limitAccess(3);
+access();
+access();
+access();
+access();
+
+// 🎯 Goal: Ek aisi function banao jo kisi doosre function (fn) ko sirf ek hi baar chalne de. Agar tum us function ko dobara call karo, to wo dobara run na ho — bas bole "Already executed".
+function sayHello() {
+    console.log("Hello!");
+}
+
+function once(fn){
+    let executed = false;
+    return function(){
+        if(executed===true){
+            console.log("alredy executed");
+        } else{
+            fn();
+            executed = true;
+        }
+    }
+}
+
+const runOnce = once(sayHello);
+runOnce();
+runOnce();
+runOnce();
+
+// 🎯 Goal: Create a function factory that can generate custom loggers — each with its own prefix (like [ERROR], [INFO], [DEBUG], etc.).
+function createLogger(prefix){
+    return function(message){
+        if(prefix === "ERROR") return console.log(`[ERROR]: ${message}`);
+        else if(prefix === "INFO") return console.log(`[INFO]: ${message}`);
+        else if(prefix === "DEBUG") return console.log(`[DEBUG]: ${message}`);
+    }
+}
+
+const errorLog = createLogger("ERROR");
+const debugLog = createLogger("INFO");
+const infoLog = createLogger("DEBUG");
+
+errorLog("something went wrong");
+debugLog("fix dynamic message");
+infoLog("message");
+
+//  Goal: Create a function factory that returns functions capable of multiplying any number by a fixed factor — using closures.
+function makeMultiplier(factor){
+    return function(num){
+        return num * factor;
+    }
+}
+
+const double = makeMultiplier(2);
+const triple = makeMultiplier(3);
+const tenTimes = makeMultiplier(10);
+
+console.log(double(5));   // 10
+console.log(triple(4));   // 12
+console.log(tenTimes(6)); // 60
+
+// 🎯 Goal: Create a function that uses closure to hide a secret number, and allows the user to keep guessing until they find the correct one.
+function secretGame(secretNumber){
+    return function(num){
+        if(num === secretNumber) return console.log("correct");
+        else return console.log("try again");
+    }
+}
+const guess = secretGame(7);
+guess(3); // Try again!
+guess(9); // Try again!
+guess(7); // Correct!
+
+// Create a stopwatch using closures — where you can start and stop the timer, and it will calculate how much time passed between the two actions.
+function stopwatch(){
+    let startTime = null;
+    let endTime = null;
+    return function(action){
+        if(action === "start"){
+            startTime = Date.now();
+        } else if(action === "stop"){
+            endTime = Date.now();
+            let difference = (endTime - startTime)/1000;
+            return console.log(`Elapsed time: ${difference} seconds`);
+        }
+    }
+}
+
+const timer = stopwatch();
+timer("start");
+
+setTimeout(() => {
+  timer("stop");
+}, 3000);
